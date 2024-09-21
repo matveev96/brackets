c6929da1617c5106f48b3af6197873466ed2ef67
matveev96 <sasha.matveev1996@icloud.com>
2024-09-21 14:48:12 +0300

    feat: brackets solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  const stack = [];
+  const openBrackets = new Set();
+  const bracketPairs = new Map();
+
+  for (const [open, close] of bracketsConfig) {
+      openBrackets.add(open);
+      bracketPairs.set(open, close);
+  }
+
+  for (const char of str) {
+      if (openBrackets.has(char)) {
+          if (bracketPairs.get(char) === char && stack.length && stack[stack.length - 1] === char) {
+              stack.pop();
+          } else {
+              stack.push(char);
+          }
+      } else {
+          const lastOpen = stack.pop();
+          if (bracketPairs.get(lastOpen) !== char) {
+              return false;
+          }
+      }
+  }
+  return stack.length === 0;
 }
